3ad72b1fa1de9431f36ea70b0a329af047e320ef
miidlaj <mumidlaj@gmail.com>
2022-12-17 05:55:34 +0530

    environment

--- EcommerceCommon/src/main/java/com/ecommerce/common/Constants.java
+++ EcommerceCommon/src/main/java/com/ecommerce/common/Constants.java
@@ -5,9 +5,9 @@ public class Constants {
     public static final String S3_BASE_URI;
 
     static {
-        String bucketName =  "commercefox"; //System.getProperty("AWS_BUCKET_NAME");
+        String bucketName = System.getProperty("AWS_BUCKET_NAME"); //"commercefox";
         System.out.println("bucket Name: " + bucketName);
-        String region = "ap-south-1";//System.getProperty("AWS_REGION");
+        String region = System.getProperty("AWS_REGION"); //"ap-south-1";
         System.out.println("Region : " + region);
         String pattern = "https://%s.s3.%s.amazonaws.com";
         S3_BASE_URI = bucketName == null ? "" : String.format(pattern, bucketName, region);;
